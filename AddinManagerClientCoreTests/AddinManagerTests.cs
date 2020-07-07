7e087ac1dd44f4eb7db03892de881df1ffd0cca1
Xarlot <maxim.litvinov@devexpress.com>
2020-07-07 10:00:02 +0300

    fx

--- AddinManagerClientCoreTests/AddinManagerTests.cs
+++ AddinManagerClientCoreTests/AddinManagerTests.cs
@@ -4,8 +4,5 @@ using NUnit.Framework;
 namespace AddinManagerClientCoreTests {
     [TestFixture]
     public class AddinManagerTests {
-        [Test]
-        public void FirstTest() {
-        }
     }
 }
